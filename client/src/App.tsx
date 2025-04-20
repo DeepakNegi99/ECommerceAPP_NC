09a82454dd23ec2bec1a3908308e6f87f64ee5d6
deepak <deepak99negi@gmail.com>
2025-04-20 21:54:01 +0530

    LinkedAPItoFrontEnd
    LinkedAPItoFrontEnd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import {useEffect, useState} from "react";
+
 
 function App() {
- 
+  const [products, setProducts] = useState<{name: string, price:number}[]>([]);
 
+  useEffect(() => {
+   fetch('https://localhost:5001/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+  }, []);
+
+  const addProduct = () => {
+    setProducts(prevState => [...prevState, { name: `Product ${prevState.length + 1}`, price: Math.random() * 100 }]);
+  };
+
+  
   return (
-   <div style={{fontSize: '1.6rem'}}>ReStore</div>
-  )
+    <div>
+      <h1>ReStore</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
