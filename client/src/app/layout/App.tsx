ffe3008d3aa355ec4433f71bfd8551102e9c2b26
Deepak Negi <deepak99negi@gmail.com>
2025-10-03 20:41:26 +0530

    Add React Router and implement app routing
    Integrated react-router-dom for client-side routing. Refactored App and Catalog components to support route-based rendering. Added new pages (Home, About, Contact, ProductDetails) and a central router configuration. Updated NavBar to use navigation links and improved layout. Entry point now uses RouterProvider for route management.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,49 +1,47 @@
-import { useEffect, useState } from "react";
-import { Catalog } from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
+import { useState } from "react";
 import NavBar from "./NavBar";
-import { Box, Container, createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import {
+  Box,
+  Container,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
   const [darkMode, setDarkMode] = useState(false);
   const palletType = darkMode ? "dark" : "light";
-
-    const toggleDarkMode = () => {
-       setDarkMode(!darkMode);
-    }
 
   const theme = createTheme({
     palette: {
       mode: palletType,
       background: {
-        default: palletType === "light" ? "#eaeaea": "#121212" ,
+        default: palletType === "light" ? "#eaeaea" : "#121212",
       },
     },
   });
 
-  useEffect(() => {
-    fetch("https://localhost:5001/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
-  }, []);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+        <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
         <Box
           sx={{
             minHeight: "100vh",
-            backgroundColor: darkMode ?  
-            'radial-gradient(circle, #1e3a4a, #111B27)' :
-            'radial-gradient(circle, #baecf9, #f0f9ff)',
+            backgroundColor: darkMode
+              ? "radial-gradient(circle, #1e3a4a, #111B27)"
+              : "radial-gradient(circle, #baecf9, #f0f9ff)",
             py: 6,
           }}
         >
           <Container maxWidth="xl" sx={{ mt: 8 }}>
-            <Catalog products={products} />
+            <Outlet />
           </Container>
         </Box>
       </ThemeProvider>
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,5 +1,34 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import { DarkMode, LightMode } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
+
+const navStyle = {
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": { color: "grey.500" },
+  "&.active": { color: "#baecf9" },
+};
 
 type Props = {
   darkMode: boolean;
@@ -9,11 +38,39 @@ type Props = {
 const NavBar = ({ darkMode, toggleDarkMode }: Props) => {
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6">RE-STORE</Typography>
-        <IconButton color="inherit" onClick={toggleDarkMode}>
-          {darkMode ? <DarkMode /> : <LightMode sx={{ color: "orange" }} />}
-        </IconButton>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <Box display={"flex"} alignItems="center">
+          <Typography component={NavLink} sx={navStyle} to="/" variant="h6">
+            RE-STORE
+          </Typography>
+          <IconButton color="inherit" onClick={toggleDarkMode}>
+            {darkMode ? <DarkMode /> : <LightMode sx={{ color: "orange" }} />}
+          </IconButton>
+        </Box>
+       
+          <List sx={{ display: "flex" }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyle}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+          
+          <Box display={"flex"} alignItems="center">
+          <IconButton size="large">
+            <Badge>
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} sx={navStyle} key={path}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
